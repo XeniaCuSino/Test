d7b2ce10319309821353deaeec61cb2e50068794
Ксения Кусь <kuzovkovaks_ot@mail.ru>
2022-04-17 22:19:24 +0300

    life

--- Views/NewsTemplate.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include('..\models\News.php');
-$new = $_GET['news'];
-?>
-<h3><?php echo $new->getTitle();?></h3>
-<p><?php echo $new->getAn_nt();?></p>
-<p><?php echo $new->getText();?></p>
-<p><b>Теги:</b> <?php echo $new->getTags();?></p>
-<h5><?php echo $new->getDate();?></h5>
