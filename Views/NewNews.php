81d2f4dccd856c706fcee1acfe726ff89aaaaaaf
Ксения Кусь <kuzovkovaks_ot@mail.ru>
2022-04-17 18:39:03 +0300

    News

---
+++ Views/NewNews.php
@@ -0,0 +1,7 @@
+<form action="/create" method="post">
+    <input type="text" name="title" placeholder="Заголовок">
+    <input type="text" name="anons" placeholder="Анонс">
+    <textarea name="text">Текст новости</textarea>
+    <input type="text" name="tags" placeholder="Теги">
+    <input type="submit" name="send" value="Создать">
+</form>
